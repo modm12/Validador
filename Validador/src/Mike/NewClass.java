e8574b5e536e2f6ca58d8d772b34f78e5089407f
PC-Ciudad Deseada <PC-Ciudad Deseada@PC-Server>
2014-03-15 23:23:29 -0600

    Primer acceso

---
+++ Validador/src/Mike/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mike;
+
+/**
+ *
+ * @author PC-Ciudad Deseada
+ */
+public class NewClass {
+    
+}
